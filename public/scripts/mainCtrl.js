fb26be0f2ab2811bb5b74241ee62f472372bc862
gleb-svechnikov <gleb.svechnikov@gmail.com>
2014-06-15 17:04:58 +0400

    Add categories + add fonts and some styles

--- public/scripts/mainCtrl.js
+++ public/scripts/mainCtrl.js
@@ -1,4 +1,13 @@
-function mainCtrl($scope) {
+function mainCtrl($scope, $http) {
+	$http({method: 'GET', url: '/category/list'}).
+    success(function(data, status, headers, config) {
+    	$scope.cetegories = data;
+    
+    }).
+   	error(function(data, status, headers, config) {
+    });
+    
+
   $scope.museums = [
     {
     	id: 1,
@@ -6,12 +15,64 @@ function mainCtrl($scope) {
     	image:"http://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Imperial_Academy_of_Arts.jpg/640px-Imperial_Academy_of_Arts.jpg"
     },
     {id: 2, title:'Menshikov palace', image:"http://upload.wikimedia.org/wikipedia/commons/d/d9/Menshikov_palace.jpg"},
-    {id: 3, title: 'Marble Palace', image: "http://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/AlexandreIIImonument.jpg/1024px-AlexandreIIImonument.jpg"}
+    {id: 3, title: 'Marble Palace', image: "http://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/AlexandreIIImonument.jpg/1024px-AlexandreIIImonument.jpg"},
+    {
+    	id: 1,
+    	title:'Russian Academy of Arts', 
+    	image:"http://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Imperial_Academy_of_Arts.jpg/640px-Imperial_Academy_of_Arts.jpg"
+    },
+    {id: 2, title:'Menshikov palace', image:"http://upload.wikimedia.org/wikipedia/commons/d/d9/Menshikov_palace.jpg"},
+    {id: 3, title: 'Marble Palace', image: "http://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/AlexandreIIImonument.jpg/1024px-AlexandreIIImonument.jpg"},
+    {
+    	id: 1,
+    	title:'Russian Academy of Arts', 
+    	image:"http://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Imperial_Academy_of_Arts.jpg/640px-Imperial_Academy_of_Arts.jpg"
+    },
+    {id: 2, title:'Menshikov palace', image:"http://upload.wikimedia.org/wikipedia/commons/d/d9/Menshikov_palace.jpg"},
+    {id: 3, title: 'Marble Palace', image: "http://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/AlexandreIIImonument.jpg/1024px-AlexandreIIImonument.jpg"},
+    {
+    	id: 1,
+    	title:'Russian Academy of Arts', 
+    	image:"http://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Imperial_Academy_of_Arts.jpg/640px-Imperial_Academy_of_Arts.jpg"
+    },
+    {id: 2, title:'Menshikov palace', image:"http://upload.wikimedia.org/wikipedia/commons/d/d9/Menshikov_palace.jpg"},
+    {id: 3, title: 'Marble Palace', image: "http://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/AlexandreIIImonument.jpg/1024px-AlexandreIIImonument.jpg"},
+    {
+    	id: 1,
+    	title:'Russian Academy of Arts', 
+    	image:"http://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Imperial_Academy_of_Arts.jpg/640px-Imperial_Academy_of_Arts.jpg"
+    },
+    {id: 2, title:'Menshikov palace', image:"http://upload.wikimedia.org/wikipedia/commons/d/d9/Menshikov_palace.jpg"},
+    {id: 3, title: 'Marble Palace', image: "http://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/AlexandreIIImonument.jpg/1024px-AlexandreIIImonument.jpg"},
+    {
+    	id: 1,
+    	title:'Russian Academy of Arts', 
+    	image:"http://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Imperial_Academy_of_Arts.jpg/640px-Imperial_Academy_of_Arts.jpg"
+    },
+    {id: 2, title:'Menshikov palace', image:"http://upload.wikimedia.org/wikipedia/commons/d/d9/Menshikov_palace.jpg"},
+    {id: 3, title: 'Marble Palace', image: "http://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/AlexandreIIImonument.jpg/1024px-AlexandreIIImonument.jpg"},
+    {
+    	id: 1,
+    	title:'Russian Academy of Arts', 
+    	image:"http://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Imperial_Academy_of_Arts.jpg/640px-Imperial_Academy_of_Arts.jpg"
+    },
+    {id: 2, title:'Menshikov palace', image:"http://upload.wikimedia.org/wikipedia/commons/d/d9/Menshikov_palace.jpg"},
+    {id: 3, title: 'Marble Palace', image: "http://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/AlexandreIIImonument.jpg/1024px-AlexandreIIImonument.jpg"},
+    {
+    	id: 1,
+    	title:'Russian Academy of Arts', 
+    	image:"http://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Imperial_Academy_of_Arts.jpg/640px-Imperial_Academy_of_Arts.jpg"
+    },
+    {id: 2, title:'Menshikov palace', image:"http://upload.wikimedia.org/wikipedia/commons/d/d9/Menshikov_palace.jpg"},
+    {id: 3, title: 'Marble Palace', image: "http://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/AlexandreIIImonument.jpg/1024px-AlexandreIIImonument.jpg"},
     ];
+    
+/*
   $scope.cetegories = [
   	{id: 1, name: "Fine art"},
   	{id: 2, name: "Science and Natural History"},
   	{id: 3, name: "Biographical"},
   	{id: 4, name: "Local History"},
   ]
+*/
 }
